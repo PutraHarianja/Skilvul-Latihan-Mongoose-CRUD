ed9367ab2467a597ece2a4f508e09668d55a00bf
PutraHarianja <putra.harianja007@gmail.com>
2021-12-10 09:21:48 +0700

    refactor: create router folder

---
+++ index.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+const express = require('express')
+
+const router = require('./routes')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.use(router)
+
+app.listen(port, () => {
+    console.log('server is listening on', port)
+})
